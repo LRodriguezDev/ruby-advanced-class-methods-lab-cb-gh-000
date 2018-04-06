2349381699d2527262ec00771eff52a8a50c5e52
Luis Rodriguez <zacha@tutanota.com>
2018-04-06 23:20:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,40 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.detect{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    new_song = self.find_by_name(song_name)
+    if new_song != nil
+      new_song
+    else
+      new_song = self.create_by_name(song_name)
+      new_song
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort
+  end
 end
